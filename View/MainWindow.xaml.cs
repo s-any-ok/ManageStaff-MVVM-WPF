d16a204c88f760620bb63fa4734c90de3f6f383f
Oleksandr Levak (laptop) <sashavytvyckyj@gmail.com>
2021-05-04 18:32:39 +0300

    Start Project, added folders (View, Model, ViewModel)

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ManageStaff
+namespace ManageStaff.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
